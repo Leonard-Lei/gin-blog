1497a1ded50af3faada6eac951e72cf570a3469f
leijiwu <1287279970@qq.com>
2019-08-04 01:09:15 +0800

    增加J表单验证、WT身份验证、日志系统、Swagger API文档

---
+++ models/article.go
@@ -0,0 +1,104 @@
+package models
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+type Article struct {
+	Model
+	//gorm:index，用于声明这个字段为索引，如果你使用了自动迁移功能则会有所影响，在不使用则无影响
+	TagID int `json:"tag_id" gorm:"index"`
+	//Tag字段，实际是一个嵌套的struct，它利用TagID与Tag模型相互关联，在执行查询的时候，能够达到Article、Tag关联查询的功能
+	Tag Tag `json:"tag"`
+
+	Title      string `json:"title"`
+	Desc       string `json:"desc"`
+	Content    string `json:"content"`
+	CreatedBy  string `json:"created_by"`
+	ModifiedBy string `json:"modified_by"`
+	State      int    `json:"state"`
+}
+
+func ExistArticleByID(id int) bool {
+	var article Article
+	db.Select("id").Where("id = ?", id).First(&article)
+
+	if article.ID > 0 {
+		return true
+	}
+
+	return false
+}
+
+func GetArticleTotal(maps interface{}) (count int) {
+	db.Model(&Article{}).Where(maps).Count(&count)
+
+	return
+}
+
+/*
+Preload就是一个预加载器，它会执行两条SQL，分别是SELECT * FROM blog_articles;和SELECT * FROM blog_tag WHERE id IN (1,2,3,4);
+那么在查询出结构后，gorm内部处理对应的映射逻辑，将其填充到Article的Tag中，会特别方便，并且避免了循环查询
+那么有没有别的办法呢，大致是两种
+gorm的Join
+循环Related
+*/
+func GetArticles(pageNum int, pageSize int, maps interface{}) (articles []Article) {
+	db.Preload("Tag").Where(maps).Offset(pageNum).Limit(pageSize).Find(&articles)
+
+	return
+}
+
+/*
+Article是如何关联到Tag
+能够达到关联，首先是gorm本身做了大量的约定俗成
+Article有一个结构体成员是TagID，就是外键。gorm会通过类名+ID的方式去找到这两个类之间的关联关系
+Article有一个结构体成员是Tag，就是我们嵌套在Article里的Tag结构体，我们可以通过Related进行关联查询
+
+*/
+func GetArticle(id int) (article Article) {
+	db.Where("id = ?", id).First(&article)
+	db.Model(&article).Related(&article.Tag)
+
+	return
+}
+
+func EditArticle(id int, data interface{}) bool {
+	db.Model(&Article{}).Where("id = ?", id).Updates(data)
+
+	return true
+}
+
+func AddArticle(data map[string]interface{}) bool {
+	db.Create(&Article{
+		TagID:     data["tag_id"].(int),
+		Title:     data["title"].(string),
+		Desc:      data["desc"].(string),
+		Content:   data["content"].(string),
+		CreatedBy: data["created_by"].(string),
+		State:     data["state"].(int),
+	})
+
+	return true
+}
+
+func DeleteArticle(id int) bool {
+	db.Where("id = ?", id).Delete(Article{})
+
+	return true
+}
+
+func (article *Article) BeforeCreate(scope *gorm.Scope) error {
+	//time.Now().Unix() 返回当前的时间戳
+	scope.SetColumn("CreatedOn", time.Now().Unix())
+
+	return nil
+}
+
+func (article *Article) BeforeUpdate(scope *gorm.Scope) error {
+	scope.SetColumn("ModifiedOn", time.Now().Unix())
+
+	return nil
+}
---
+++ routers/router.go
@@ -0,0 +1,55 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+
+	_ "gin-blog/docs"
+
+	"gin-blog/middleware/jwt"
+	"gin-blog/pkg/setting"
+	"gin-blog/routers/api"
+	v1 "gin-blog/routers/api/v1"
+)
+
+func InitRouter() *gin.Engine {
+	r := gin.New()
+
+	r.Use(gin.Logger())
+
+	r.Use(gin.Recovery())
+
+	gin.SetMode(setting.RunMode)
+
+	//新增获取token的方法
+	r.GET("/auth", api.GetAuth)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
+	{
+		//获取标签列表
+		apiv1.GET("/tags", v1.GetTags)
+		//新建标签
+		apiv1.POST("/tags", v1.AddTag)
+		//更新指定标签
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		//删除指定标签
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+
+		//获取文章列表
+		apiv1.GET("/articles", v1.GetArticles)
+		//获取指定文章
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		//新建文章
+		apiv1.POST("/articles", v1.AddArticle)
+		//更新指定文章
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		//删除指定文章
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
+	}
+
+	return r
+}
