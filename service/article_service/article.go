836663f90dc3821d3144291d7ef13521f9976fb8
leijiwu <1287279970@qq.com>
2019-08-12 12:26:23 +0800

    新增详情页跳转

--- service/article_service/article.go
+++ service/article_service/article.go
@@ -1,12 +1,7 @@
 package article_service
 
 import (
-	"encoding/json"
-
 	"gin-blog/models"
-	"gin-blog/pkg/gredis"
-	"gin-blog/pkg/logging"
-	"gin-blog/service/cache_service"
 )
 
 type Article struct {
@@ -58,26 +53,26 @@ func (a *Article) Edit() error {
 }
 
 func (a *Article) Get() (*models.Article, error) {
-	var cacheArticle *models.Article
+	//var cacheArticle *models.Article
 
-	cache := cache_service.Article{ID: a.ID}
-	key := cache.GetArticleKey()
-	if gredis.Exists(key) {
-		data, err := gredis.Get(key)
-		if err != nil {
-			logging.Info(err)
-		} else {
-			json.Unmarshal(data, &cacheArticle)
-			return cacheArticle, nil
-		}
-	}
+	// cache := cache_service.Article{ID: a.ID}
+	// key := cache.GetArticleKey()
+	// if gredis.Exists(key) {
+	// 	data, err := gredis.Get(key)
+	// 	if err != nil {
+	// 		logging.Info(err)
+	// 	} else {
+	// 		json.Unmarshal(data, &cacheArticle)
+	// 		return cacheArticle, nil
+	// 	}
+	// }
 
 	article, err := models.GetArticle(a.ID)
 	if err != nil {
 		return nil, err
 	}
 
-	gredis.Set(key, article, 3600)
+	//gredis.Set(key, article, 3600)
 	return article, nil
 }
 
